5bb9d0234d3f088f61e4b954055a7566ece5af46
Paúl Tumbaco <paultumbaco@gmail.com>
2022-03-18 17:43:42 +0800

    checked retrieve

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,14 +1,24 @@
-from brownie import accounts, config,SimpleStorage
-#import os
+#from numpy import sometrue
+from brownie import accounts, config, SimpleStorage
+import os
 def deploy_simple_storage():
     account = accounts[0]
-    SimpleStorage.deploy({"from":account})
     # accountPaul = accounts.load("paulkey")
-    # print(account)
     # print(accountPaul)
     #account = accounts.add(os.getenv("PRIVATE_KEY"))
     #account =accounts.add(config["wallets"]["from_key"])
-    print(account)
+    print("account: ",account)
+    simple_storage = SimpleStorage.deploy({"from":account})
+    print(simple_storage)
+    storedValue = simple_storage.retrieve()
+    print(storedValue)
+    transaction = simple_storage.store(35,{"from": account})
+    transaction.wait(1)
+    print("transaction:",transaction)
+    updateValue = simple_storage.retrieve()
+    print("updated  value :",updateValue)
+
+
 def main():
     print("initializating..")
     deploy_simple_storage()
